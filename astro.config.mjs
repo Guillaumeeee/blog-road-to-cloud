bb5b0310d61df83299fa7acabb57f5ae2f593492
Guillaumeeee <gspestre@gmail.com>
2023-02-12 22:29:24 +0100

    🔧 add Github Action for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://guillaumeeee.github.io',
+	base: 'road-to-cloud',
 	integrations: [mdx(), sitemap()],
 });
